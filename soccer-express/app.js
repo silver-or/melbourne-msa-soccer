c319bc942140fefa4cf3beab19291b8ad414b1c7
silver-or <laura817@naver.com>
2022-03-29 18:16:52 +0900

    2022-03-29 18:16

--- soccer-express/app.js
+++ soccer-express/app.js
@@ -1,22 +1,33 @@
 require('dotenv').config()
+var cors = require('cors')
 const express = require('express') // import // 'express' : key, key는 패키지의 이름. 패키지는 package.json에 있음.
 const mongoose = require('mongoose')
 
 const app = express()
 const { PORT, MONGO_URI } = process.env
 
-app.get('/', (req, res) => {
-  res.json({"현재 시간 : " : new Date().toLocaleString()})
-})
-
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
+app.use(cors())
+
+var corsOptions = {
+  origin : 'http://localhost:3000',
+  optionsSuccessStatus: 200
+}
 
 mongoose
   .connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Successfully connected to mongodb'))
   .catch(e => console.error(e))
+
+app.get('/', (req, res) => {
+  res.json({"현재 시간 : " : new Date().toLocaleString()})
+})
+
+app.get('/api/now', cors(corsOptions), (req, res) => {
+  res.json({"now" : new Date().toLocaleString()})
+})
 
 app.listen(PORT,() => {
   console.log({"현재 시간 : " : new Date().toLocaleString()})
