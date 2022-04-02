753b05a85a1fc1a89aba0c1eec034eae9dc436fc
silver-or <laura817@naver.com>
2022-04-03 02:36:32 +0900

    2022-04-03 02:36 next.config

--- soccer-next/next.config.js
+++ soccer-next/next.config.js
@@ -1,11 +1,3 @@
-module.exports = {
-  server: {
-    port: 8000,
-    host: '0.0.0.0',
-    timing: false
-  }
-}
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
