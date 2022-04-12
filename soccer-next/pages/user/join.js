435481e41fad252b98f883c0d50b1125a17549e0
silver-or <laura817@naver.com>
2022-04-12 17:55:26 +0900

    2022-04-12 17:55

--- soccer-next/pages/user/join.js
+++ soccer-next/pages/user/join.js
@@ -1,41 +1,269 @@
-import axios from "axios"
-import React, {useState} from "react"
+import { yupResolver } from '@hookform/resolvers/yup';
+import { styled, darken } from '@mui/material/styles';
+import { useDispatch } from 'react-redux'
+import { motion } from 'framer-motion';
+import { Controller, useForm } from 'react-hook-form';
+import Button from '@mui/material/Button';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import TextField from '@mui/material/TextField';
+import Typography from '@mui/material/Typography';
+import Link from 'next/link';
+import Image from 'next/image'
+import * as yup from 'yup';
+import _ from '../@lodash';
+// import './style/UserLayout.scss'
+// import './style/UserRegister.scss'
+// import { CheckList } from '..';
+import { joinRequest } from '../../redux/reducers/user.reducer';
 
-export default function Join(){
-    const proxy = 'http://localhost:5000'
-    const [inputs, setInputs] = useState({}) // 상태를 inputs 라는 이름으로 줌, JSON 내부의 어느 공간에 이름을 준 것. sign-in과 다른 이름이어야 함.
+const schema = yup.object().shape({
+    userId : yup.string().required('You must enter your id'),
+    pw: yup
+    .string()
+    .required('Please enter your password.')
+    .min(8, 'Password is too short - should be 8 chars minimum.'),
+    pwConfirm : yup.string().oneOf([yup.ref('pw'), null], 'Passwords must match'),
+    userName : yup.string().required('You must enter your name'),
+    tel : yup.string().required('You must enter your telephone number.')
+});
 
-    const handleChange = e => {
-        e.preventDefault()
-        const {name, value} = e.target
-        setInputs({...inputs, [name] : value})
-    }
+const defaultValues = {
+    userId : '',
+    pw : '',
+    userName : '',
+    tel : ''
+};
 
-    const handleSubmit = e => {
-        e.preventDefault()
-        axios.post(proxy + '/api/user/sign-up', inputs)
-        .then(res => {
-            alert(`${JSON.stringify(inputs)}`)
-            alert(`${JSON.stringify(res.data)}`)
-        })
-        .catch(err => alert(err))   
-    }
+export default function Register3Page() {
+  const { control, formState, handleSubmit, reset } = useForm({
+    mode: 'onChange',
+    defaultValues,
+    resolver: yupResolver(schema),
+  });
+  const dispatch = useDispatch()
 
-    return (<>
-        <h1>회원가입 폼</h1>
-        <form onSubmit={handleSubmit}>
-            <div>
-                <label><b>사용자 ID</b></label> <br/>
-                <input type="text" name="userId" onChange={handleChange}/> <br/>
-                <label><b>비밀번호</b></label> <br/>
-                <input type="password" name="pw" onChange={handleChange}/> <br/>
-                <label><b>이름</b></label> <br/>
-                <input type="text" name="userName" onChange={handleChange}/> <br/>
-                <label><b>전화번호</b></label> <br/>
-                <input type="text" name="tel" onChange={handleChange}/> <br/>
-                <button>회원가입하기</button>
+  const { isValid, dirtyFields, errors } = formState;
+
+  function onSubmit() {
+    reset(defaultValues);
+  }
+
+  return (
+    <>
+      <div className="User-container">
+          <motion.div
+            initial={{ opacity: 0, scale: 0.6 }}
+            animate={{ opacity: 1, scale: 1 }}
+          >
+            <Card
+              square
+            >
+              <CardContent>
+                <motion.div
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 1, transition: { delay: 0.2 } }}
+                >
+                  <div className="flex items-center mb-48">
+                    <Image src="/user/paper-pencil.png" alt="me" width="64" height="64" />
+                    <div className="border-l-1 mr-4 w-1 h-40" />
+                    <div>
+                      <Typography className="text-24 font-semibold logo-text" color="inherit">
+                      </Typography>
+                      <Typography
+                        className="text-16 tracking-widest -mt-8 font-700"
+                        color="textSecondary"
+                      >
+                        회원가입
+                      </Typography>
+                    </div>
+                  </div>
+                </motion.div>
+                <form
+                  name="registerForm"
+                  noValidate
+                  className="flex flex-col justify-center w-full"
+                  onSubmit={handleSubmit(async (data) => { await dispatch(joinRequest({ ...data, })) })}
+                >
+                  <Controller
+                    name="username"
+                    control={control}
+                    render={({ field }) => (
+                      <TextField
+                        {...field}
+                        className="mb-16"
+                        label="Name"
+                        autoFocus
+                        type="username"
+                        error={!!errors.username}
+                        helperText={errors?.username?.message}
+                        variant="outlined"
+                        required
+                        fullWidth
+                      />
+                    )}
+                  />
+                  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+
+
+                  <Controller
+                    id='email'
+                    name="email"
+                    control={control}
+                    render={({ field }) => (
+                      <TextField
+                        {...field}
+                        className="mb-16"
+                        label="Email"
+                        type="email"
+                        error={!!errors.email}
+                        helperText={errors?.email?.message}
+                        variant="outlined"
+                        required
+                        fullWidth
+                      />
+                    )}
+                  />
+                  <button onClick={() => dispatch(
+                    exist(document.getElementById('email').value))}>중복체크</button>
+                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+
+                  <Controller
+                    name="phone"
+                    control={control}
+                    render={({ field }) => (
+                      <TextField
+                        {...field}
+                        className="mb-16"
+                        label="Phone"
+                        type="text"
+                        error={!!errors.phone}
+                        helperText={errors?.phone?.message}
+                        variant="outlined"
+                        required
+                        fullWidth
+                      />
+                    )}
+                  />
+                  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+
+                  <Controller
+                    name="birth"
+                    control={control}
+                    render={({ field }) => (
+                      <TextField
+                        {...field}
+                        className="mb-16"
+                        label="Birth"
+                        type="text"
+                        error={!!errors.birth}
+                        helperText={errors?.birth?.message}
+                        variant="outlined"
+                        required
+                        fullWidth
+                      />
+                    )}
+                  />
+                  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                  <Controller
+                    name="address"
+                    control={control}
+                    render={({ field }) => (
+                      <TextField
+                        {...field}
+                        className="mb-16"
+                        label="address"
+                        type="address"
+                        error={!!errors.address}
+                        helperText={errors?.address?.message}
+                        variant="outlined"
+                        fullWidth
+                      />
+                    )}
+                  />
+                  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                  <Controller
+                    name="password"
+                    control={control}
+                    render={({ field }) => (
+                      <TextField
+                        {...field}
+                        className="mb-16"
+                        label="Password"
+                        type="password"
+                        error={!!errors.password}
+                        helperText={errors?.password?.message}
+                        variant="outlined"
+                        required
+                        fullWidth
+                      />
+                    )}
+                  />
+                  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                  <Controller
+                    name="passwordConfirm"
+                    control={control}
+                    render={({ field }) => (
+                      <TextField
+                        {...field}
+                        className="mb-16"
+                        label="passwordConfirm"
+                        type="passwordConfirm"
+                        error={!!errors.password}
+                        helperText={errors?.password?.message}
+                        variant="outlined"
+                        required
+                        fullWidth
+                      />
+                    )}
+                  />
+                  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                    <Button style={{'margin-top' : '60px'}}
+                    variant="contained"
+                    color="primary"
+                    className="w-full mx-auto mt-16"
+                    aria-label="Register"
+                    disabled={_.isEmpty(dirtyFields) || !isValid}
+                    type="submit"
+                  >
+                    Create an account
+                  </Button>
+                </form>
+              </CardContent>
+
+
+
+              <div className="flex flex-col items-center justify-center pb-32">
+                <span className="font-normal">이미 회원이신가요?</span>
+                <Link className="font-normal" href="/user/Login">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                  로그인하기
+                </Link>
+              
+              </div>
+              
+            </Card>
+
+
+            <div className="Register3-rightSection hidden md:flex flex-1 items-center justify-center p-64">
+              <div className="max-w-320">
+                <motion.div
+                  initial={{ opacity: 0, y: 40 }}
+                  animate={{ opacity: 1, y: 0, transition: { delay: 0.2 } }}
+                >
+                </motion.div>
+                <motion.div
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 1, transition: { delay: 0.3 } }}
+                >
+                </motion.div>
+                
+              </div>
             </div>
-            <div><span id="result-span"/></div>
-        </form>
-    </>)
+          </motion.div>
+        </div>
+        {/* <div style={{marginTop: "-442px"}}>
+        <CheckList/>
+        </div> */}
+    </>
+  );
 }
